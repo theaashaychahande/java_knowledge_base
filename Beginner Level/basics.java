a6c6ed271561f8c697209860537a344eb463cce6
Aashay Chahande <aashaychahande1717@gmail.com>
2025-09-22 14:58:15 +0530

    Update basics.java

--- Beginner Level/basics.java
+++ Beginner Level/basics.java
@@ -1,1 +1,30 @@
+/**
+=== DEFINITION ===
+The foundational program that prints "Hello, World!" to introduce Java's basic structure.
+=== WHY USE ===
+To verify your JDK setup and understand the mandatory structure of a Java application.
 
+=== CODE FORMAT ===
+public class ClassName {
+    public static void main(String[] args) {
+        System.out.println("Text to output");
+    }
+}
+
+=== CODE EXAMPLE ===
+*/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Aashay");
+        System.out.print(" Svpcet");
+      
+    }
+}
+
+/**
+=== OUTPUT ===
+Hello, World!
+Aashay 
+Svpcet 
+*/
